5934835d4aae83e6e4cae4d6753126c1c7e6eb8f
Vitor Magnago <avmagnago@gmail.com>
2023-03-09 09:55:49 -0300

    feat: create files structure and tailwind custom classes

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				grey: {
+					background: "#D5E1EF",
+					font: "#7D889E",
+				},
+				Navy: {
+					dark: "#1F314F",
+				},
+			},
+			fontFamily: {
+				inter: ["Outfit", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
